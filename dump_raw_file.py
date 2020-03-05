3107e7c6eecb87efd0008d47416424c03434f3cb
Subrahmanyam8k <subrahmanyam.c@8kmiles.com>
2020-03-05 11:49:57 +0530

    first commit

--- dump_raw_file.py
+++ dump_raw_file.py
@@ -1,13 +1,7 @@
-import gzip
-import io
-import os
-import random
-import sys
-import time
-import traceback
-import json
-import urlparse
-import urllib
-import uuid
-import shutil
-import copy
+
+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
